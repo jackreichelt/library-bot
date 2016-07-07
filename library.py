30a1a6062a7b22a1bc54f05fc4bde586bebed712
jackreichelt <jackreichelt@gmail.com>
2016-07-07 14:35:53 +1000

    Created library bot

---
+++ library.py
@@ -0,0 +1,99 @@
+from datetime import datetime
+from pytz import timezone
+
+class Library(object):
+
+  users = {} #format: {user id: user object}
+
+  def __init__(self):
+    subs = open('borrowers.txt')
+    for line in subs:
+      chunks = line.split(',')
+
+      user_details = chunks[0].split(':')
+      users[user_details[0]] = User(user_details[1])
+
+      for book in chunks[1:]:
+        book = book.split(':')
+        self.borrow_book(user_details[0], user_details[1], book[0], book[1])
+
+  def borrow_book(self, user_id, user_name, book, date = None):
+    if user_id not in self.users:
+      self.users[user_id] = User(user_name)
+
+    self.users[user_id].borrow_book(book, date)
+
+  def return_book(self, user_id, book):
+    if user_id not in self.users:
+      return -1
+
+    if self.users[user_id].return_book(book) == -2:
+      return -2
+
+  def all_borrowed_books(self):
+    out = ''
+
+    for user_id, user in self.users.items():
+      if user.count() > 0:
+        out += '<@{}> has borrowed:\n{}\n'.format(user_id, user.list_books())
+
+    return out
+
+  def count(self):
+    total = 0
+
+    for user in self.users.values():
+      total += user.count()
+
+    return total
+
+  def write_library(self):
+    f = open('borrowers.txt')
+
+    for user_id, user in self.users.items():
+      if user.count() > 0:
+        f.write('{}:{}'.format(user_id, user.write()))
+
+class User(object):
+
+  name = ''
+  borrowed_books = {}
+
+  def __init__(self, name):
+    self.name = name
+    self.borrowed_books = {}
+
+  def borrow_book(self, book, date = None):
+    if book in self.borrowed_books:
+      return -1
+
+    if date == None:
+      self.borrowed_books[book] = datetime.now(timezone('Australia/Sydney')).date()
+    else:
+      self.borrowed_books[book] = datetime.strptime(date, '%Y/%m/%d').date() #format is yyyy/mm/dd, e.g. 2016/04/15, for April 15th, 2016.
+
+  def return_book(self, book):
+    if book not in self.borrowed_books:
+      return -2
+
+    self.borrowed_books.pop(book)
+
+  def list_books(self):
+    out = ''
+    current_date = datetime.now(timezone('Australia/Sydney')).date()
+
+    for book, date in self.borrowed_books.items():
+      out += '    {}, borrowed {} days ago.\n'.format(book, (current_date-date).days)
+
+    return out
+
+  def count(self):
+    return len(self.borrowed_books)
+
+  def write(self):
+    out = '{}'.format(self.name)
+
+    for book, date in self.borrowed_books.items():
+      out += ',{}:{}'.format(book, datetime.strftime(date, '%Y/%m/%d'))
+
+    return out
---
+++ librarybot.py
@@ -0,0 +1,117 @@
+from time import sleep
+from slackclient import SlackClient
+import os
+from datetime import datetime
+from library import Library
+from pytz import timezone
+import atexit
+from tinys3 import Connection
+
+NAME = 'library_bot'
+
+posted = False
+
+lib = Library()
+channels = {} #Mapping from channel_id to user_id
+
+@atexit.register
+def save_library():
+  print('Writing library.')
+  lib.write_library()
+  conn.upload('borrowers.txt', open('borrowers.txt', 'rb'), 'iit-library')
+
+usage = """
+Welcome to the IIT Library!
+
+You can checkout a book like this. Supplying a username will borrow it for that person.
+`libcheckout <book name> [@<username>]`
+You can return a book like this. Supplying a username will return it for that person.
+`libcheckin <book name> [@<username>]`
+You can see a list of all borrowed books like this. This will also happen once a week automatically
+`library`
+
+Anything else and I'll show you this message to help you out!
+
+If you have any facts you want to add, comments, complaints, or bug reports, message Jack Reichelt.
+"""
+
+token = os.environ.get('TOKEN', None) # found at https://api.slack.com/web#authentication
+sc = SlackClient(token)
+if sc.rtm_connect() == True:
+  print('Connected.')
+
+  sc.api_call("im.list")
+
+  while True:
+    response = sc.rtm_read()
+    for part in response:
+      print(part)
+      if 'ims' in part:
+        channels = part['ims']
+      if part['type'] == 'message' and 'text' in part:
+        words = part['text'].split()
+
+        # Borrowing a book for another user
+        if words[0] == 'libcheckout' and words[-1].startswith('<@'):
+          user = sc.server.users.find(words[-1][2:-1])
+          if user == None:
+            sc.api_call("chat.postMessage", channel=part['channel'], text="That user doesn't exist. Try tagging them with @.", username=NAME, icon_emoji=':book:')
+          else:
+            user_id = user.id
+            username = user.real_name
+            lib.borrow_book(user_id, username, ' '.join(words[1:-1]))
+            sc.api_call("chat.postMessage", channel=part['channel'], text="{} has borrowed {}.".format(username, ' '.join(words[1:-1])), username=NAME, icon_emoji=':book:')
+
+        # Borrowing a book for themselves
+        elif words[0] == 'libcheckout':
+          user_id = part['user']
+          username = sc.api_call("users.info", user=user_id)['user']['profile']['real_name']
+
+          lib.borrow_book(user_id, username, ' '.join(words[1:]))
+          sc.api_call("chat.postMessage", channel=part['channel'], text="{} has borrowed {}.".format(username, ' '.join(words[1:])), username=NAME, icon_emoji=':book:')
+
+        # Returning a book for another user
+        elif words[0] == 'libcheckin' and words[-1].startswith('<@'):
+          lib_response = lib.return_book(words[-1][2:-1], ' '.join(words[1:-1]))
+
+          if lib_response == -1:
+            sc.api_call("chat.postMessage", channel=part['channel'], text="That user doesn't have any books borrowed.", username=NAME, icon_emoji=':book:')
+          elif lib_response == -2:
+            sc.api_call("chat.postMessage", channel=part['channel'], text="That user hasn't borrowed that book.", username=NAME, icon_emoji=':book:')
+          else:
+            sc.api_call("chat.postMessage", channel=part['channel'], text="{} has been returned.".format(words[1:-1]), username=NAME, icon_emoji=':book:')
+
+        # Returning a book for themselves
+        elif words[0] == 'libcheckin':
+          lib_response = lib.return_book(part['user'], ' '.join(words[1:]))
+
+          if lib_response == -1:
+            sc.api_call("chat.postMessage", channel=part['channel'], text="You don't have any books borrowed.", username=NAME, icon_emoji=':book:')
+          elif lib_response == -2:
+            sc.api_call("chat.postMessage", channel=part['channel'], text="You haven't borrowed that book.", username=NAME, icon_emoji=':book:')
+          else:
+            sc.api_call("chat.postMessage", channel=part['channel'], text="{} has been returned.".format(words[1:-1]), username=NAME, icon_emoji=':book:')
+
+        elif len(words) == 1 and words[0] == 'library':
+          if lib.count():
+            sc.api_call("chat.postMessage", channel=part['channel'], text=lib.all_borrowed_books(), username=NAME, icon_emoji=':book:')
+          else:
+            sc.api_call("chat.postMessage", channel=part['channel'], text='There are no borrowed books.', username=NAME, icon_emoji=':book:')
+
+        elif ('library' in words and 'help' in words) or 'libraryhelp' in words:
+          sc.api_call("chat.postMessage", channel=part['channel'], text=usage, username=NAME, icon_emoji=':book:')
+
+        save_library()
+
+    # bookclub channel is C0QPDRHD2
+    if 0 <= datetime.now(timezone('Australia/Sydney')).time().hour < 1 and posted == True: #midnight to 1am
+      print('It\'s a new day.')
+      posted = False
+    if 9 <= datetime.now(timezone('Australia/Sydney')).time().hour < 10 and posted == False and datetime.now(timezone('Australia/Sydney')).weekday() == 3: #3pm to 5pm
+      print('It\'s cat fact time!')
+      posted = True
+      sc.api_call("chat.postMessage", channel='C0QPDRHD2', text=lib.all_borrowed_books(), username=NAME, icon_emoji=':book:')
+
+    sleep(1)
+else:
+  print('Connection Failed, invalid token?')
